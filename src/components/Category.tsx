14375d2187bf64660ef83fbca5f3fa744728fb76
OltiRocka <olti@roka.dev>
2023-10-27 19:05:28 +0200

    Added animation to categories

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -7,7 +7,36 @@ import { categories } from "../hooks_data/Data";
 const Category = () => {
   const isMobile = useIsMobile();
   const [activeCategory, setActiveCategory] = useState<number | null>(null);
+  const [animationClass, setAnimationClass] = useState("");
+  const buttonDropRef = useRef<HTMLDivElement>(null);
 
+  function handleLogoClick(index: number) {
+    if (activeCategory !== null) {
+      if (activeCategory === index) {
+        // The dropdown is currently visible and the same category was clicked.
+        // We need to hide the dropdown, so animate upwards
+        setAnimationClass(styles.animate_up);
+
+        setTimeout(() => {
+          setActiveCategory(null);
+        }, 500); // matches animation duration
+      } else {
+        // A different category was clicked while the dropdown is visible.
+        // First, animate upwards as if the current category content is going away.
+        setAnimationClass(styles.animate_up);
+
+        setTimeout(() => {
+          // Once the upwards animation is done, set the active category to the new index and animate downwards.
+          setActiveCategory(index);
+          setAnimationClass(styles.animate_down);
+        }, 500); // this delay gives the impression of a transition between categories
+      }
+    } else {
+      // The dropdown is currently hidden and needs to be shown, so animate downwards
+      setAnimationClass(styles.animate_down);
+      setActiveCategory(index);
+    }
+  }
   return (
     <>
       <div className={styles.categories}>
@@ -22,27 +51,26 @@ const Category = () => {
             text={category.text}
             url={category.url}
             isActive={isMobile && activeCategory === index}
-            onLogoClick={() => {
-              setActiveCategory((prev) => {
-                const newIndex = prev === index ? null : index;
-                console.log("Setting active category to", newIndex);
-                return newIndex;
-              });
-            }}
+            onLogoClick={() => handleLogoClick(index)}
           />
         ))}
       </div>
-      {isMobile &&
-        activeCategory !== null &&
-        categories[activeCategory].text.map((item, index) => (
-          <a
-            key={index}
-            href={categories[activeCategory].url[index]}
-            className={styles.button}
-          >
-            {item}
-          </a>
-        ))}
+      {isMobile && activeCategory !== null && (
+        <div
+          className={`${styles.button_drop} ${animationClass}`}
+          ref={buttonDropRef}
+        >
+          {categories[activeCategory].text.map((item, index) => (
+            <a
+              key={index}
+              href={categories[activeCategory].url[index]}
+              className={styles.button}
+            >
+              {item}
+            </a>
+          ))}
+        </div>
+      )}
     </>
   );
 };
